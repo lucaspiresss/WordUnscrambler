3df219931f7b683bcc4187fe6fed5b6ab6d275c3
lucaspiresss <lucas_pires@outlook.com>
2020-01-04 15:22:37 +0100

    Updating WordMatcher

--- WordUnscrambler/Workers/WordMatcher.cs
+++ WordUnscrambler/Workers/WordMatcher.cs
@@ -10,7 +10,45 @@ namespace WordUnscrambler.Workers
         {
             var matchedWords = new List<MatchedWord>();
 
+            foreach (var scrambledWord in scrambledWords)
+            {
+                foreach (var word in wordList)
+                {
+                    if (scrambledWord.Equals(word, StringComparison.OrdinalIgnoreCase))
+                    {
+                        matchedWords.Add(BuildMatchedWord(scrambledWord, word));
+                    }
+                    else
+                    {
+                        var scrambledWordArray = scrambledWord.ToCharArray();
+                        var wordArray = word.ToCharArray();
+
+                        Array.Sort(scrambledWordArray);
+                        Array.Sort(wordArray);
+
+                        var sortedScrambledWord = new string(scrambledWordArray);
+                        var sortedWord = new string(wordArray);
+
+                        if (sortedScrambledWord.Equals(sortedWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            matchedWords.Add(BuildMatchedWord(scrambledWord, word));
+                        }
+                    }
+                }
+            }
+
             return matchedWords;
+        }
+
+        private MatchedWord BuildMatchedWord(string scrambledWord, string word)
+        {
+            MatchedWord matchedWord = new MatchedWord
+            {
+                ScrambledWord = scrambledWord,
+                Word = word
+            };
+
+            return matchedWord;
         }
     }
 }
