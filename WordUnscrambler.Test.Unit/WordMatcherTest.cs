31adfbc9be5b5a2b96299672423bc74e75852e8c
lucaspiresss <lucas_pires@outlook.com>
2020-01-04 15:47:34 +0100

    Setting up test project up

---
+++ WordUnscrambler.Test.Unit/WordMatcherTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordUnscrambler.Test.Unit
+{
+    [TestClass]
+    public class WordMatcherTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
