e049e0bf4bd59006d8db3f070a879474f4e211ff
lucaspiresss <lucas_pires@outlook.com>
2020-01-04 14:50:44 +0100

    Creating program core structure

--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WordUnscrambler
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			bool continueWordUnsramble = true;
+
+			do
+			{
+				Console.WriteLine("Please enter input option [F (file) / M (manual)]: ");
+				var option = Console.ReadLine() ?? string.Empty;
+
+				switch (option.ToUpper())
+				{
+					case "F":
+						Console.Write("Enter scrambled words file: ");
+						ExecuteScrambledWordsInFileScenario();
+						break;
+
+					case "M":
+						Console.Write("Enter scrambled words manually: ");
+						ExecuteScrambledWordsManualEntryScenario();
+						break;
+
+					default:
+						Console.WriteLine("Option was not recognized");
+						break;
+				}
+
+				var continueDecision = string.Empty;
+				do
+				{
+					Console.WriteLine("Do you want to continue [Y(yes) / N(no)] ? ");
+					continueDecision = Console.ReadLine() ?? string.Empty;
+				} while (
+					!continueDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) &&
+					!continueDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+				continueWordUnsramble = continueDecision.Equals("N", StringComparison.OrdinalIgnoreCase);
+			} while (continueWordUnsramble);
+		}
+
+		private static void ExecuteScrambledWordsManualEntryScenario()
+		{
+		}
+
+		private static void ExecuteScrambledWordsInFileScenario()
+		{
+		}
+	}
 }
